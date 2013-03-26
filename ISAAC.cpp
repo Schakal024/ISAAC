bc1e6e7773e94189b16eae623b47bcfab5e567e5
Daniel Steiner <daniel.steiner@univie.ac.at>
2013-03-26 01:49:59 +0100

    test

--- ISAAC.cpp
+++ ISAAC.cpp
@@ -1,17 +1,53 @@
-//============================================================================
-// Name        : ISAAC.cpp
-// Author      : Daniel Steiner
-// Version     :
-// Copyright   : Not formulated yet
-// Description : (I)mplicit (S)olving (A)rchictecture for
-// 			     (A)strophysical (C)omputations
-//				 written in C, Ansi-style
-//============================================================================
+/*---------------------------------------------------------------------------
+ *
+ *
+ * 	I S A A C
+ *
+ *
+ *---------------------------------------------------------------------------
+ * Name
+ * 		ISAAC.cpp
+ *
+ * Author
+ * 		Daniel Steiner
+ *
+ * Created on
+ *		25.03.2013
+ *
+ * Version
+ * 		v1.0
+ *
+ * Copyright
+ * 		Not formulated yet
+ *
+ * Description
+ * 		(I)mplicit (S)olving (A)rchictecture for
+ * 		(A)strophysical (C)omputations
+ *
+ *		written in C, Ansi-style
+ *
+ * License
+ * 		This file is part of OpenFOAM.
+ *
+ *		ISAAC++ is free software: you can redistribute it and/or modify it
+ *		under the terms of the GNU General Public License as published by
+ *		the Free Software Foundation, either version 3 of the License, or
+ *		(at your option) any later version.
+ *
+ *		ISAAC++ is distributed in the hope that it will be useful, but WITHOUT
+ *		ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *		FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *		for more details.
+ *
+ *		You should have received a copy of the GNU General Public License
+ *		along with ISAAC++.  If not, see <http://www.gnu.org/licenses/>.
+ *-------------------------------------------------------------------------*/
+
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
-	puts("");
+	puts("Hello World");
 	return EXIT_SUCCESS;
 }
